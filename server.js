e918c77c14bd4deec81b02152f85b7b13acfcb36
naufalseira <kingnaufal3@gmail.com>
2025-01-15 02:15:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! - v13.0');
+  res.send('Hello World! - v14.0');
 });
 
 app.listen(PORT, HOST, () => {
